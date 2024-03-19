aff0c85c8f38e685b9873ab552184ca34fe34cff
artyoomi <zuodleq@gmail.com>
2024-03-19 14:19:07 +0300

    Fix problem with yadro_hello.c file

--- add_layer.sh
+++ add_layer.sh
@@ -3,9 +3,11 @@ cd poky
 source oe-init-build-env ~/build
 cd ..
 bitbake-layers create-layer meta-yadro_hello
+
+mkdir meta-yadro_hello/recipes-example/example/files
+mv yadro_hello.c meta-yadro_hello/recipes-example/example/files/yadro_hello.c
+cat compile_instr.txt > meta-yadro_hello/recipes-example/example/example_0.1.bb
+
 cd build
 bitbake-layers add-layer ../meta-yadro_hello
 cd ..
-
-cat compile_instr.txt > meta-yadro_hello/recipes-example/example/example_0.1.bb
-cp yadro_hello.c meta-yadro_hello/recipes-example/example/
