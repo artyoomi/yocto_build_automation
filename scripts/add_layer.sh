283ddda5eb9aa4ef8785142e9141ac83f8a45c0b
artyoomi <zuodleq@gmail.com>
2025-02-14 18:51:46 +0300

    Add shell check and fix .sh scripts

--- scripts/add_layer.sh
+++ scripts/add_layer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # Init poky build environment
-cd ${PROJECT_DIR}/poky/build
+cd "${PROJECT_DIR}/poky/build"
 
 # Add new layer in poky directory
-cp -r ${PROJECT_DIR}/layers/meta-custom ..
+cp -r "${PROJECT_DIR}/layers/meta-custom" ..
 
 # Add meta-custom to bblayer.conf
 bitbake-layers add-layer ../meta-custom
 
 # Add yadro-hello program to loca.conf
-echo "IMAGE_INSTALL:append = \" yadro-hello\"" >> ${PROJECT_DIR}/poky/build/conf/local.conf
+echo "IMAGE_INSTALL:append = \" yadro-hello\"" >> "${PROJECT_DIR}/poky/build/conf/local.conf"
--- scripts/run_yocto.sh
+++ scripts/run_yocto.sh
@@ -6,9 +6,9 @@ set -e
 # Export necessary environment variables
 source .env
 
-cd ${PROJECT_DIR}
+cd "${PROJECT_DIR}"
 
-sudo chown -R $USER:$USER /home/$USER
+sudo chown -R "${USER}:${USER}" "/home/${USER}"
 
 cd poky
 
@@ -25,17 +25,17 @@ elif [ "$1" == "build" ]; then
 	echo "Start building..."
 	source oe-init-build-env
 
-	if ! [ -d ${PROJECT_DIR}/poky/meta-custom/ ]; then
+	if ! [ -d "${PROJECT_DIR}/poky/meta-custom/" ]; then
 		echo ""
 		echo "Adding meta-custom layer..."
-		${PROJECT_DIR}/scripts/add_layer.sh
+		"${PROJECT_DIR}/scripts/add_layer.sh"
 		echo "meta-custom layer was succesfully added!"
 		echo "yadro-hello package was added to local.conf"
 		echo ""
 	fi
 
 	# <TODO>: maybe different way to add yadro_hello package in image
-	# if ! [ -f ${PROJECT_DIR}/poky/build/tmp/work/qemux86_64-poky-linux/core-image-minimal/1.0-r0/rootfs/usr/bin/yadro-hello ]; then
+	# if ! [ -f "${PROJECT_DIR}/poky/build/tmp/work/qemux86_64-poky-linux/core-image-minimal/1.0-r0/rootfs/usr/bin/yadro-hello" ]; then
 	# 	echo ""
 	# 	echo "yadro-hello not found in image binaries!!!"
 	# 	echo "Adding yadro-hello in image..."
